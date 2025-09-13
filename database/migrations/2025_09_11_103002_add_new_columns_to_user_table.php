a4da4d259d5a499e0d808e193d84d3a7aa0543a9
Basliel-Amsalu <acychappiekiller@gmail.com>
2025-09-13 16:26:59 +0300

    add profile info columnst to users table

---
+++ database/migrations/2025_09_11_103002_add_new_columns_to_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->nullable()->after('name');
+            $table->string('last_name')->nullable()->after('first_name');   
+            $table->enum('role', ['admin', 'space_manager', 'member'])->default('member')->after('password');
+            $table->string('phone')->nullable()->after('role');
+            $table->string('address')->nullable()->after('phone');
+            $table->string('profile_photo')->nullable()->after('address');
+            $table->date('date_of_birth')->nullable()->after('profile_photo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['first_name', 'last_name', 'role', 'phone', 'address', 'profile_photo', 'date_of_birth']);
+        });
+    }
+};
