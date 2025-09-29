4a851bf1a5dabf82b2c909bda54c764a4f723c6e
Basliel-Amsalu <acychappiekiller@gmail.com>
2025-09-29 09:10:29 +0300

    user data

--- app/Data/UserData.php
+++ app/Data/UserData.php
@@ -3,15 +3,30 @@
 namespace App\Data;
 
 use App\Enums\RoleEnum;
+use DateTime;
+use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\DataCollection;
 
 class UserData extends Data
 {
     public function __construct(
         //
+        public ?string $pid,
         public string $name,
+        public ?string $first_name,
+        public ?string $last_name,
         public string $email,
-        public string $password,
-        public RoleEnum $role,
+        // public RoleEnum $role,
+        public ?string $phone,
+        public ?string $address,
+        public ?string $profile_photo,
+        public ?string $date_of_birth,
+        public ?DateTime $email_verified_at = null,
+        #[DataCollectionOf(SpaceData::class)]
+        public ?DataCollection $spaces = null,
+        #[DataCollectionOf(BookingData::class)]
+        public ?DataCollection $bookings = null,
+
     ) {}
 }
